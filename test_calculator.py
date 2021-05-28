7c2b31c2d3bed5d7f0db7e5d8dfffc95e92d6e15
Daisy <ramired2@oregonstate.edu>
2021-05-27 17:24:40 -0700

    fixed linting errors

--- test_calculator.py
+++ test_calculator.py
@@ -6,13 +6,13 @@ import calculator
 
 class TestCalculatorApp:
     def test_add(self):
-        assert  5 == calculator.add(3, 2)
+        assert 5 == calculator.add(3, 2)
 
     def test_subtract(self):
-        assert  1 == calculator.subtract(3, 2)
+        assert 1 == calculator.subtract(3, 2)
 
     def test_multiply(self):
-        assert  6 == calculator.multiply(3, 2)
+        assert 6 == calculator.multiply(3, 2)
 
     def test_divide(self):
-        assert  2 == calculator.divide(4, 2)
+        assert 2 == calculator.divide(4, 2)
